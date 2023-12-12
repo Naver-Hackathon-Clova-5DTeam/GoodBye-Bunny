15084fcf74a21099ed78965de4568aff107736b6
Gotprgmer <gofiction95@gmail.com>
2023-12-12 15:43:13 +0900

    [INIT] project init setting

---
+++ BackEnd/goodbyebunny/src/main/java/com/clova/hackathon/goodbyebunny/GoodbyebunnyApplication.java
@@ -0,0 +1,13 @@
+package com.clova.hackathon.goodbyebunny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodbyebunnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodbyebunnyApplication.class, args);
+	}
+
+}
---
+++ BackEnd/goodbyebunny/src/test/java/com/clova/hackathon/goodbyebunny/GoodbyebunnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clova.hackathon.goodbyebunny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodbyebunnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
