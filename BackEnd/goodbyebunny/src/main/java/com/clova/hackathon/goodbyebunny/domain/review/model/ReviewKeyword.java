456435779c5a5269a891de21d871f9901fcc20b2
Gotprgmer <gofiction95@gmail.com>
2023-12-12 18:40:34 +0900

    [UPDATE] ERD 매핑

--- BackEnd/goodbyebunny/src/main/java/com/clova/hackathon/goodbyebunny/domain/review/model/ReviewKeyword.java
+++ BackEnd/goodbyebunny/src/main/java/com/clova/hackathon/goodbyebunny/domain/review/model/ReviewKeyword.java
@@ -1,2 +1,29 @@
-package com.clova.hackathon.goodbyebunny.domain.review.model;public class ReviewComment {
+package com.clova.hackathon.goodbyebunny.domain.review.model;
+
+
+import com.clova.hackathon.goodbyebunny.domain.keyword.model.Keyword;
+import com.clova.hackathon.goodbyebunny.global.common.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Table(name = "review_keyword")
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "club_member_id"))
+@AttributeOverride(name = "createdDate", column = @Column(name = "club_member_created_date"))
+@AttributeOverride(name = "updatedDate", column = @Column(name = "club_member_updated_date"))
+public class ReviewKeyword extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "keyword_id")
+    private Keyword keyword;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
 }
