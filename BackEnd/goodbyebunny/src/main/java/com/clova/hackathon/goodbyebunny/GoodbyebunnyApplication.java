2047434e669131d66b59d4e570572ed7938592e9
김예린 <yerin1198@naver.com>
2023-12-13 03:33:01 +0900

    [Fix] EnableJpaAuditing 추가

--- BackEnd/goodbyebunny/src/main/java/com/clova/hackathon/goodbyebunny/GoodbyebunnyApplication.java
+++ BackEnd/goodbyebunny/src/main/java/com/clova/hackathon/goodbyebunny/GoodbyebunnyApplication.java
@@ -2,8 +2,10 @@ package com.clova.hackathon.goodbyebunny;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GoodbyebunnyApplication {
 
 	public static void main(String[] args) {
