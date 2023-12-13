c58bdd73ec5622b6e170e384d1108b88e41bfe3b
Gotprgmer <gofiction95@gmail.com>
2023-12-13 21:41:25 +0900

    [ADD] 회고 조회 기능

--- BackEnd/goodbyebunny/src/main/java/com/clova/hackathon/goodbyebunny/domain/review/app/ReviewService.java
+++ BackEnd/goodbyebunny/src/main/java/com/clova/hackathon/goodbyebunny/domain/review/app/ReviewService.java
@@ -2,9 +2,10 @@ package com.clova.hackathon.goodbyebunny.domain.review.app;
 
 import com.clova.hackathon.goodbyebunny.domain.member.dao.MemberRepository;
 import com.clova.hackathon.goodbyebunny.domain.member.model.Member;
-import com.clova.hackathon.goodbyebunny.domain.review.api.request.ReviewContentRequest;
+import com.clova.hackathon.goodbyebunny.domain.review.api.request.ReviewCreateRequest;
+import com.clova.hackathon.goodbyebunny.domain.review.api.response.ReviewReadResponse;
 import com.clova.hackathon.goodbyebunny.domain.review.model.Review;
-import com.clova.hackathon.goodbyebunny.domain.review.repository.ReviewRepository;
+import com.clova.hackathon.goodbyebunny.domain.review.dao.ReviewRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,7 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
 
     // Review 작성
-    public ResponseEntity<Long> createReview(String memberNickname, ReviewContentRequest request) {
+    public ResponseEntity<Long> createReview(String memberNickname, ReviewCreateRequest request) {
 
         Optional<Member> member = memberRepository.findMemberByNickname(memberNickname);
 
@@ -33,6 +34,25 @@ public class ReviewService {
         Long savedCommentId = reviewRepository.save(review).getId();
 
         return ResponseEntity.ok(savedCommentId);
+
+    }
+
+    // Review 조회
+    public ResponseEntity<ReviewReadResponse> getReview(String memberNickname, Long reviewId) {
+
+        //review 조회
+        Review review = reviewRepository.findReviewById(reviewId)
+                .orElseThrow(() -> new IllegalStateException("존재하지 않는 회고입니다."));
+        //member 조회
+        Member member = memberRepository.findMemberByNickname(memberNickname)
+                .orElseThrow(() -> new IllegalStateException("존재하지 않는 회원입니다."));
+
+        //review과 memberId의 유효성 체크
+        if (review.getMember().getId() != member.getId()){
+            throw new IllegalStateException("허가되지 않은 접근입니다.");
+        }
+
+        return ResponseEntity.ok(ReviewReadResponse.of(review.getTitle(), review.getContent(),review.getUpdatedDate()));
 
     }
 }
--- BackEnd/goodbyebunny/src/main/java/com/clova/hackathon/goodbyebunny/domain/review/dao/ReviewRepository.java
+++ BackEnd/goodbyebunny/src/main/java/com/clova/hackathon/goodbyebunny/domain/review/dao/ReviewRepository.java
@@ -1,9 +1,15 @@
-package com.clova.hackathon.goodbyebunny.domain.review.repository;
+package com.clova.hackathon.goodbyebunny.domain.review.dao;
 
 import com.clova.hackathon.goodbyebunny.domain.member.model.Member;
 import com.clova.hackathon.goodbyebunny.domain.review.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, Long> {}
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Optional<Review> findReviewById(Long reviewId);
+
+
+}
