3a6672d384803492ae599be25e76e9b55162fbc6
Gotprgmer <gofiction95@gmail.com>
2023-12-16 20:54:32 +0900

    [ADD] 초기 아이템 저장

--- BackEnd/goodbyebunny/src/main/java/com/clova/hackathon/goodbyebunny/domain/review/dao/ReviewKeywordRepository.java
+++ BackEnd/goodbyebunny/src/main/java/com/clova/hackathon/goodbyebunny/domain/review/dao/ReviewKeywordRepository.java
@@ -1,10 +1,16 @@
 package com.clova.hackathon.goodbyebunny.domain.review.dao;
 
+import com.clova.hackathon.goodbyebunny.domain.review.model.Review;
 import com.clova.hackathon.goodbyebunny.domain.review.model.ReviewKeyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ReviewKeywordRepository extends JpaRepository<ReviewKeyword, Long> {
+    Optional<List<ReviewKeyword>> findReviewKeywordsByReviewId(Long reviewId);
+
 
 }
